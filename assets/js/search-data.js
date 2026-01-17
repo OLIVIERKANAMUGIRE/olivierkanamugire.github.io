1207c6f9bb21506c34f7bfb76f295f04f806d0d6
OLIVIERKANAMUGIRE <145745191+OLIVIERKANAMUGIRE@users.noreply.github.com>
2026-01-17 10:57:30 +0000

    Deploying to gh-pages from @ OLIVIERKANAMUGIRE/olivierkanamugire.github.io@5289447dbf9d7554930fda504896581dbb6e1abb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,8 +45,8 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          title: "Teaching",
+          description: "African Institute for Mathematical Sciences.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
